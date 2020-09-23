fbf094b6185b6d712046469252b817e2393f4de5
Abhishek Kumar <40334524+ak9504496@users.noreply.github.com>
2020-09-24 00:00:28 +0530

    for loop to render dictionary items

--- demo.py
+++ demo.py
@@ -4,9 +4,10 @@ app = Flask(__name__)
 
 
 
-@app.route('/hello/<int:score>')
-def hello_name(score):
-    return render_template('hello.html', marks=score)
+@app.route('/result')
+def result():
+    dict = { 'phy':50, 'che':60, 'maths':70}
+    return render_template('result.html', result=dict)
 
 
 if __name__ == '__main__':
